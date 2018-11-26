2f9d83929e6a8786933baa6803192dc0bbd806cc
Christian Mehlmauer <firefart@gmail.com>
2018-11-26 17:35:08 +0100

    upgrade packages

--- decision/build.bat
+++ decision/build.bat
@@ -1,8 +1,8 @@
 @echo off
 
 rmdir /S /Q skill
-pip install -r requirements.txt -t skill/
-pip install --user babel
+pip install -r requirements.txt -t skill/ --upgrade
+pip install --user babel --upgrade
 pybabel extract data.py -o locales/base.pot
 pybabel update -i locales/base.pot -d locales
 pybabel compile -d locales
