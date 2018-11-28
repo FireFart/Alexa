05203e16e566ca8a36eca92c5a9a39cc1c6f8a16
Christian Mehlmauer <firefart@gmail.com>
2018-11-28 13:36:25 +0100

    update

--- decision/data.py
+++ decision/data.py
@@ -5,7 +5,7 @@ from gettext import gettext as _
 WELCOME_MESSAGE = _("Welcome to decision maker. Please ask your question.")
 WELCOME_RETRY = _("Please ask your question.")
 COPYRIGHT = _("copyright by firefart")
-HELP_MESSAGE = _("Please ask a policy related question")
+HELP_MESSAGE = _("Please ask a policy related question like is it allowed to disabled windows updates?")
 EXCEPTION_MESSAGE = _("Sorry, there was some problem. Please try again.")
 FALLBACK_MESSAGE = _("Sorry, I don't know how to interpret that. Please try again.")
 
